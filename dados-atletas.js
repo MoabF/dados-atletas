51561a0f1a7b1bf89881d92faaae296aec9a956c
Moab Varela <109436938+MoabF@users.noreply.github.com>
2026-01-11 17:23:23 -0300

    Update dados-atletas.js
    criação do construtor

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,1 +1,19 @@
-
+class Atleta {
+    constructor(nome, idade, peso, altura, notas){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+    calculaCategoria(){}
+    calculaIMC(){}
+    calculaMediaValida(){}
+    obtemNomeAtleta(){}
+    obtemIdadeAtleta(){}
+    obtemPesoAtleta(){}
+    obtemNotasAtleta(){}
+    obtemCategoria(){}
+    obtemIMC(){}
+    obtemMediaValida(){}
+}
