03b4d094f804fe5c555a5eaf3258d4958587463b
Moab Varela <109436938+MoabF@users.noreply.github.com>
2026-01-11 17:29:02 -0300

    Update dados-atletas.js
    implementação do metodo calcular categoria

--- dados-atletas.js
+++ dados-atletas.js
@@ -6,7 +6,23 @@ class Atleta {
         this.altura = altura;
         this.notas = notas;
     }
-    calculaCategoria(){}
+    calculaCategoria(){
+        if(idade >= 9 && idade <= 11){
+            return "Infantil"
+        }
+        else if(idade >= 12 && idade <= 13){
+            return "Juvenil"
+        }
+        else if(idade >= 14 && idade <= 15){
+            return "Intermediário"
+        }
+        else if(idade >= 16 && idade <= 30){
+            return "Adulto"
+        }
+        else{
+            return "Sem categoria"
+        }
+    }
     calculaIMC(){}
     calculaMediaValida(){}
     obtemNomeAtleta(){}
